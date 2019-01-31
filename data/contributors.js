af4b5fe0f7684334309dcd16cdb5a6f912f68687
Tejas Kumar <tejas@contiamo.com>
2019-01-31 01:53:39 +0100

    Add contributor data

--- data/contributors.js
+++ data/contributors.js
@@ -1,32 +1,34 @@
-export const onecamp = {
-  name: 'onecamp',
-  link: 'https://github.com/onecamp',
-  photoURL: 'https://avatars0.githubusercontent.com/u/1410520?s=460&v=4'
-}
-export const tyriar = {
-  name: 'tyriar',
-  link: 'https://twitter.com/tyriar',
-  photoURL:
-    'https://pbs.twimg.com/profile_images/776617605389877248/aYSZBtVe_400x400.jpg'
-}
-export const jerch = {
-  name: 'jerch',
-  link: 'https://github.com/jerch',
-  photoURL: 'https://avatars2.githubusercontent.com/u/11927490?s=200&v=4'
-}
-export const stanzilla = {
-  name: 'stanzilla',
-  link: 'https://github.com/stanzilla',
-  photoURL: 'https://avatars0.githubusercontent.com/u/75278?s=460&v=4'
-}
-export const chabou = {
-  name: 'chabou',
-  link: 'https://github.com/chabou',
-  photoURL: 'https://avatars3.githubusercontent.com/u/4137761?s=460&v=4'
-}
-export const tejaskumar = {
-  name: 'tejaskumar_',
-  link: 'https://twitter.com/tejaskumar_',
-  photoURL:
-    'https://pbs.twimg.com/profile_images/1068684634647605248/pdA5uTx3_400x400.jpg'
+module.exports = {
+  onecamp: {
+    name: 'onecamp',
+    link: 'https://github.com/onecamp',
+    photoURL: 'https://avatars0.githubusercontent.com/u/1410520?s=460&v=4'
+  },
+  tyriar: {
+    name: 'tyriar',
+    link: 'https://twitter.com/tyriar',
+    photoURL:
+      'https://pbs.twimg.com/profile_images/776617605389877248/aYSZBtVe_400x400.jpg'
+  },
+  jerch: {
+    name: 'jerch',
+    link: 'https://github.com/jerch',
+    photoURL: 'https://avatars2.githubusercontent.com/u/11927490?s=200&v=4'
+  },
+  stanzilla: {
+    name: 'stanzilla',
+    link: 'https://github.com/stanzilla',
+    photoURL: 'https://avatars0.githubusercontent.com/u/75278?s=460&v=4'
+  },
+  chabou: {
+    name: 'chabou',
+    link: 'https://github.com/chabou',
+    photoURL: 'https://avatars3.githubusercontent.com/u/4137761?s=460&v=4'
+  },
+  tejaskumar: {
+    name: 'tejaskumar_',
+    link: 'https://twitter.com/tejaskumar_',
+    photoURL:
+      'https://pbs.twimg.com/profile_images/1068684634647605248/pdA5uTx3_400x400.jpg'
+  }
 }
